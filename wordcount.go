76b58d83b3b8c9d8d74fc9da2615e436e276d86a
ofrolo-ya-ru <ofrolo@ya.ru>
2022-11-22 23:03:20 +0300

    done

---
+++ wordcount.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"os"
+)
+
+func main() {
+	src, err := readInput()
+	if err != nil {
+		fail(err)
+	}
+	fmt.Println(len(src))
+}
+
+// readInput reads source string
+// from command line arguments and returns them.
+func readInput() (src []string, err error) {
+	src = os.Args[1:]
+	if len(src) == 0 {
+		return src, errors.New("missing words to count")
+	}
+	return src, nil
+}
+
+// fail prints the error and exits.
+func fail(err error) {
+	fmt.Println("wordcount:", err)
+	os.Exit(1)
+}
